7c2c0da127be1d4c7dd0c25e009540537f2b0a12
Vaibhav Singh <61027919+vaibhav1997756@users.noreply.github.com>
2021-12-05 15:59:05 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,30 @@
+var calculator = {
+    'num1': parseInt(prompt("Enter the num1:")),
+    'num2': parseInt(prompt("Enter the num2:")),
+    'res': 0,
+    'sum': function() {
+        this.res = this.num1 + this.num2;
+        return this.res;
+    },
+    'sub': function() {
+        this.res = this.num1 - this.num2;
+        return this.res;
+    },
+    'mul': function() {
+        this.res = this.num1 * this.num2;
+        return this.res;
+    },
+    'div': function() {
+        this.res = this.num1 / this.num2;
+        return this.res;
+    },
+
+}
+var sumation = calculator.sum();
+console.log(sumation);
+var subtraction = calculator.sub();
+console.log(subtraction)
+var multiplication = calculator.mul();
+console.log(multiplication)
+var division = calculator.div();
+console.log(parseInt(division));
